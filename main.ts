2adfd758c95eaf3ba1545efe45697124088c91fa
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-11-06 08:48:50 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by:Mercy 
- * Created on: Oct 2025
- * This program ...
+ * Created on: Nov 2025
+ * This program moves car
 */
 
-// variable
+// varables
 let distanceOfObstacle: number = 0
 
 // clean
@@ -17,10 +17,16 @@ while (true) {
     distanceOfObstacle = sonar.ping(
         DigitalPin.P12,
         DigitalPin.P13,
-        PingUnit.Centimeters
-    )
-   if (distanceOfObstacle > 10) {
-       
-   }
-
+        PingUnit.Centimeters)
+    while (distanceOfObstacle > 10) {
+        robotbit.StpCarMove(10, 48)
+        distanceOfObstacle = sonar.ping(
+            DigitalPin.P12,
+            DigitalPin.P13,
+            PingUnit.Centimeters)
+    }
+    robotbit.StpCarMove(0, 48)
+    basic.pause(1000)
+    robotbit.StpCarMove(-10, 48)
+    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
 }
