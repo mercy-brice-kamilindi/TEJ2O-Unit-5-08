11a2eecc2897685fb86b72f2327eaae832a933ff
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-12-19 12:48:55 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,28 +5,25 @@
  * This program moves a cool car
 */
 
-// varables
-let distanceOfObstacle: number = 0
+// / variables
+let distanceToObject: number = 0
 
-// clean
+// Clean
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// car start
 while (true) {
-    distanceOfObstacle = sonar.ping(
-        DigitalPin.P12,
-        DigitalPin.P13,
-        PingUnit.Centimeters)
-    while (distanceOfObstacle > 10) {
-        robotbit.StpCarMove(10, 48)
-        distanceOfObstacle = sonar.ping(
-            DigitalPin.P12,
-            DigitalPin.P13,
-            PingUnit.Centimeters)
+    distanceToObject = sonar.ping(
+        DigitalPin.P0,
+        DigitalPin.P1,
+        PingUnit.Centimeters
+    )
+    basic.clearScreen()
+    basic.showNumber(distanceToObject)
+    if (distanceToObject < 10) {
+        basic.pause(500)
+        robotbit.StpCarMove(-10, 48)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
     }
-    robotbit.StpCarMove(0, 48)
-    basic.pause(1000)
-    robotbit.StpCarMove(-10, 48)
-    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+    robotbit.StpCarMove(10, 48)
 }
